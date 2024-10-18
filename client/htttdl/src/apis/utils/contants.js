ad00ac77ae760456bd73ce082d40e6f1360d7fdc
HuyVo1231 <vonhathuy1306@gmail.com>
2024-10-18 12:25:53 +0700

    deploy frontend

--- client/htttdl/src/apis/utils/contants.js
+++ client/htttdl/src/apis/utils/contants.js
@@ -1,3 +1,4 @@
-// export const BASE_URL = 'https://notes-api-la14.onrender.com'
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://hethongthongtin-dialy-mern.onrender.com'
+// export const BASE_URL = 'http://localhost:3000'
+
 export const GOOGLE_MAPS_API = 'AIzaSyBrw-u6bnptvtTnIuWGaigp4oQrqqkT1tI'
